5dd689604c3b1607758d8fe4481bb3e63a0252b9
mazenmostafa2025 <mazenmostafa2025@hotmail.com>
2025-05-07 17:53:26 +0300

    asd

---
+++ notification-service/src/middlewares/protect.js
@@ -0,0 +1,29 @@
+const jwt = require('jsonwebtoken');
+
+// Verify JWT token
+const verifyToken = (token) => {
+  try {
+    console.log(process.env.JWT_SECRET);
+    return jwt.verify(token, process.env.JWT_SECRET);
+  } catch (err) {
+    return null;
+  }
+};
+
+exports.protect = (req, res, next) => {
+  const token = req.headers.authorization && req.headers.authorization.split(' ')[1];
+
+  if (!token) {
+    return res.status(401).json({ message: 'No token, authorization denied' });
+  }
+
+  const decoded = verifyToken(token);
+
+  if (!decoded) {
+    return res.status(401).json({ message: 'Token is not valid' });
+  }
+
+  req.userId = decoded.id;
+  next();
+};
+
